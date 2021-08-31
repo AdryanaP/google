3693f04497ace747b1c6a94213a861be3860283f
AdryanaP <dryportuga@gmail.com>
2021-08-31 14:55:04 -0300

    acordeon estilizado e adicionado seta que transiciona

--- components/Acordeon.jsx
+++ components/Acordeon.jsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
-import styles from "./Acordeon.module.css"
+import styles from "./Acordeon.module.css";
 
 export default function Acordeon(props) {
-    const [estadoAcordeon, mudarEstadoAcordeon] = useState(false)
+  const [estadoAcordeon, mudarEstadoAcordeon] = useState(true);
 
-    const cliqueBotao = () => {
-        mudarEstadoAcordeon(valor => !valor)
-    }
+  const cliqueBotao = () => {
+    mudarEstadoAcordeon((valor) => !valor);
+  };
 
-    return (
-        <div className="acordeon">
-            <button className="btn-expandir" onClick={cliqueBotao}>{props.pergunta}</button>
-            <div className={styles.dentroAcordeon} aria-expanded={!estadoAcordeon}>
-                <p>{props.resposta}</p>
-                <a href={props.link} className="link">S
-                    <div className="url">{props.url}</div>
-                    <h3 className="titulo">{props.titulo}</h3>
-                </a>
-                <p>
-                    Pesquisar: <a href={props.link2}>{props.pergunta}</a>
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.acordeon}>
+      <button className={styles.btnExpandir} onClick={cliqueBotao}>
+        {props.pergunta}
+        <span className={estadoAcordeon ? styles.seta : styles.seta2}></span>
+      </button>
+      <div className={styles.dentroAcordeon} aria-expanded={!estadoAcordeon}>
+        <p className={styles.resposta}>{props.resposta}</p>
+        <a href={props.link} className="link">
+          <div className="url">{props.url}</div>
+          <h3 className="titulo">{props.titulo}</h3>
+        </a>
+        <p className={styles.pesquisar}>
+          Pesquisar: <a href={props.link2}>{props.pergunta}</a>
+        </p>
+      </div>
+    </div>
+  );
 }
